ec805097b672ea0e6a2b975f6e5b32f213ded0ce
Marta <martagomz@gmail.com>
2019-05-18 17:19:18 +0200

    Añadidas vistas para crear y editar los post del blog dentro del sitio

--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField('Título', max_length=200)
+    text = models.TextField('Texto')
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
